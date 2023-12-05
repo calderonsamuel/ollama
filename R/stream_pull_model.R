0ec00a72eb6010b2af8a8b4d25ebb4a05b9e9bda
Samuel Calderon <samuel.calderon@uarm.pe>
2023-12-05 17:48:30 -0500

    add a working version of stream pulling

--- R/stream_pull_model.R
+++ R/stream_pull_model.R
@@ -1,81 +1,69 @@
-#' List Local Models
-#'
-#' List models that are available locally.
-#'
-#' @return A with fields `name`, `modified_at`, and `size` for each model. 
-#'
-#' @export
-#' @examples
-#' \dontrun{
-#' ollama_list()
-#' }
-#' 
-ollama_list <- function() {
-    ollama_set_task("tags") %>% 
-        httr2::req_perform() %>% 
-        httr2::resp_body_json()
+stream_pull_model <- function(request) {
+    request_body <- request %>% 
+        purrr::pluck("body")
+    
+    request_url <- request %>% 
+        purrr::pluck("url")
+    
+    request_handle <- curl::new_handle() %>% 
+        curl::handle_setopt(postfields = body_to_json_str(request_body))
+    
+    buffer <- list(status = glue::glue("Starting pull of {request_body$data$name}"))
+    cli::cli_alert_info(buffer$status)
+    # the progress bar is not being used currently, but remains for compatibility
+    # buffer_progress_bar <- cli::cli_progress_bar(
+    #     name = "Pulling {request_body$name}", 
+    #     type = "download"
+    # )
+    
+    curl_response <- curl::curl_fetch_stream(
+        url = request_url,
+        handle = request_handle,
+        fun = stream_pull_model_callback
+    )
+    
+    httr2::response(
+        url = curl_response$url,
+        method = "POST"
+    )
 }
 
 
-#' Show Model Information
-#'
-#' Show details about a model including modelfile, template, parameters, license, and system prompt.
-#'
-#' @param model Name of the model to show
-#' @return A list. 
-#'
-#' @export
-#' @examples
-#' \dontrun{
-#' ollama_show_model_info("llama2")
-#' ollama_show_model_info("mistral")
-#' }
-#' 
-ollama_show_model_info <- function(model) {
-    body <- list(name = model)
-    ollama_set_task("show") %>% 
-        httr2::req_body_json(body) %>% 
-        httr2::req_perform() %>% 
-        httr2::resp_body_json()
+stream_pull_model_callback <- function(x) {
+    stream <- rawToChar(x)
+    # cat(stream)
+    
+    # container <- list()
+    
+    jsonlite::stream_in(
+        textConnection(stream),
+        verbose = FALSE,
+        handler = function(chunk) {
+            # container <<- c(container, list(chunk))
+            stream_pull_model_chunk_callback(chunk)
+        }
+    )
+
+    # container %>%
+    #     purrr::walk(~chunk_callback(.x, buffer, buffer_progress_bar))
+    
 }
 
-ollama_copy_model <- function(source, destination) {
-    body <- list(source = source, destination = destination)
-    response <- ollama_set_task("copy") %>% 
-        httr2::req_body_json(body) %>% 
-        httr2::req_perform()
+stream_pull_model_chunk_callback <- function(chunk, progress_bar = NULL) {
+    # For some reason some chunks have length > 1
+    if (any(chunk$status != buffer$status)) cli::cli_alert_info(chunk$status[1])
     
-    if(httr2::resp_status(response) == 200) {
-        cli::cli_alert_success("{.val {destination}} model has been added")
-    }
-}
-
-ollama_delete_model <- function(model) {
-    body <- list(name = model)
-    response <- ollama_set_task("delete") %>% 
-        httr2::req_method("DELETE") %>% 
-        httr2::req_body_json(body) %>% 
-        httr2::req_perform() 
+    # if (!rlang::is_empty(chunk$total)) {
+    #     cli::cli_progress_update(
+    #         id = progress_bar,
+    #         set = chunk$completed[1],
+    #         total = chunk$total[1]
+    #     )
+    # }
+    # 
+    # if(rlang::is_empty(chunk$total) && !rlang::is_empty(buffer$total)) {
+    #     cli::cli_progress_done(id = progress_bar)
+    # }
     
-    if(httr2::resp_status(response) == 200) {
-        cli::cli_alert_success("{.val {model}} model has been deleted")
-    }
-}
-
-ollama_pull_model <- function(model, stream = TRUE) {
-    body <- list(name = model, stream = stream)
-    
-    request <- ollama_set_task("pull") %>% 
-        httr2::req_body_json(body)
-    
-    if (stream) {
-        response <- stream_pull_model(request)
-    } else {
-        cli::cli_alert_info("This will take some time. Be patient")
-        response <- httr2::req_perform(request) 
-    }
-    
-    if(httr2::resp_status(response) == 200) {
-        cli::cli_alert_success("{.val {model}} model has been pulled")
-    }
+    buffer <<- chunk
 }
