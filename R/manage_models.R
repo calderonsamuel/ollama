8b30e6577a77d1c2065471a33255271b9e0fdd97
Samuel Calderon <samuel.calderon@uarm.pe>
2023-12-05 15:24:59 -0500

    export all model management functions

--- R/manage_models.R
+++ R/manage_models.R
@@ -2,7 +2,7 @@
 #'
 #' List models that are available locally.
 #'
-#' @return A with fields `name`, `modified_at`, and `size` for each model. 
+#' @return A list with fields `name`, `modified_at`, and `size` for each model. 
 #'
 #' @export
 #' @examples
@@ -21,7 +21,7 @@ ollama_list <- function() {
 #'
 #' Show details about a model including modelfile, template, parameters, license, and system prompt.
 #'
-#' @param model Name of the model to show
+#' @param name Name of the model to show
 #' @return A list. 
 #'
 #' @export
@@ -31,14 +31,29 @@ ollama_list <- function() {
 #' ollama_show_model_info("mistral")
 #' }
 #' 
-ollama_show_model_info <- function(model) {
-    body <- list(name = model)
+ollama_show_model_info <- function(name) {
+    body <- list(name = name)
     ollama_set_task("show") %>% 
         httr2::req_body_json(body) %>% 
         httr2::req_perform() %>% 
         httr2::resp_body_json()
 }
 
+#' Copy a Model
+#' 
+#' Copy a model. Creates a model with another name from an existing model.
+#' 
+#' @param source Name of the existing model to copy
+#' @param destination Name of the new model. Must be different from `source`.
+#' 
+#' @return None
+#' 
+#' @export
+#' @examples
+#' \dontrun{
+#' ollama_copy_model("llama2", "llama2_backup")
+#' }
+#' 
 ollama_copy_model <- function(source, destination) {
     body <- list(source = source, destination = destination)
     response <- ollama_set_task("copy") %>% 
@@ -50,20 +65,54 @@ ollama_copy_model <- function(source, destination) {
     }
 }
 
-ollama_delete_model <- function(model) {
-    body <- list(name = model)
+#' Delete a Model
+#' 
+#' Delete a model and its data.
+#' 
+#' @param name Model name to delete
+#' 
+#' @return None
+#' 
+#' @export
+#' @examples
+#' \dontrun{
+#' ollama_delete_model("llama2")
+#' }
+#' 
+ollama_delete_model <- function(name) {
+    body <- list(name = name)
     response <- ollama_set_task("delete") %>% 
         httr2::req_method("DELETE") %>% 
         httr2::req_body_json(body) %>% 
         httr2::req_perform() 
     
     if(httr2::resp_status(response) == 200) {
-        cli::cli_alert_success("{.val {model}} model has been deleted")
+        cli::cli_alert_success("{.val {name}} model has been deleted")
     }
 }
 
-ollama_pull_model <- function(model, stream = TRUE) {
-    body <- list(name = model, stream = stream)
+#' Pull a Model
+#' 
+#' Download a model from the ollama library. 
+#' Cancelled pulls are resumed from where they left off, 
+#' and multiple calls will share the same download progress.
+#' 
+#' @param name Name of the model to pull
+#' @param insecure (optional) allow insecure connections to the library. 
+#' Only use this if you are pulling from your own library during development.
+#' @param stream (optional) if FALSE the response will be returned as a single 
+#' response object, rather than a stream of objects
+#' 
+#' @return None
+#' 
+#' @export
+#' @examples 
+#' \dontrun{
+#' ollama_pull_model("mistral")
+#' ollama_pull_model("mistral", stream = FALSE)
+#' }
+ollama_pull_model <- function(name, insecure = FALSE, stream = TRUE) {
+    body <- list(name = name, insecure = insecure, stream = stream)
     
     request <- ollama_set_task("pull") %>% 
         httr2::req_body_json(body)
@@ -76,6 +125,6 @@ ollama_pull_model <- function(model, stream = TRUE) {
     }
     
     if(httr2::resp_status(response) == 200) {
-        cli::cli_alert_success("{.val {model}} model has been pulled")
+        cli::cli_alert_success("{.val {name}} model has been pulled")
     }
 }
